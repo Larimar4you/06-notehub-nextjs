daad3661d2c4c4fcbe70a886b126eb29d7b5abb0
Larimar4you <Zajanezlaja@gmail.com>
2026-04-14 01:33:13 +0200

    GO

---
+++ notehub-next/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import css from "./Header.module.css";
+
+export default function Header() {
+  return (
+    <header className={css.header}>
+      <h1>NoteHub</h1>
+    </header>
+  );
+}
---
+++ notehub-next/lib/api.ts
@@ -0,0 +1,47 @@
+import axios, { type AxiosResponse } from "axios";
+import type { Note } from "../types/note";
+
+const BASE_URL = import.meta.env.VITE_NOTEHUB_API_URL;
+const TOKEN = import.meta.env.VITE_NOTEHUB_TOKEN;
+
+const noteHubApi = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    Authorization: `Bearer ${TOKEN}`,
+  },
+});
+interface NoteResponse {
+  notes: Note[];
+  totalPages: number;
+}
+interface NewNote {
+  title: string;
+  content: string;
+  tag: string;
+}
+
+export const fetchNotes = async (
+  page: number,
+  search: string,
+): Promise<NoteResponse> => {
+  const response = await noteHubApi.get<NoteResponse>(`/notes`, {
+    params: {
+      page,
+      perPage: 12,
+      search,
+    },
+  });
+  return response.data;
+};
+
+export const createNote = async (newNote: NewNote): Promise<Note> => {
+  const response = await noteHubApi.post<Note>("/notes", newNote);
+  return response.data;
+};
+
+export const deleteNote = async (id: string): Promise<Note> => {
+  const response: AxiosResponse<Note> = await noteHubApi.delete<Note>(
+    `/notes/${id}`,
+  );
+  return response.data;
+};
